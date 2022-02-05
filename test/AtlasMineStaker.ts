88eaad39fb8f9842531e4eaaa823b75ab6bae086
millenarian <kvk0x@protonmail.com>
2022-02-05 10:04:21 -0800

    starting on testing

---
+++ test/AtlasMineStaker.ts
@@ -0,0 +1,5 @@
+import { ethers, waffle } from "hardhat";
+import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
+import { expect } from "chai";
+
+const { loadFixture } = waffle;
