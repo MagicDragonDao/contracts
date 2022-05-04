a24d6b167efbbe47f050358c3e11c726485ee816
millenarian <kvk0x@protonmail.com>
2022-05-04 14:37:37 -0700

    do drMAGIC contract

--- contracts/drMAGIC.sol
+++ contracts/drMAGIC.sol
@@ -1,35 +1,78 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.10;
+
+pragma solidity ^0.8.11;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
+/**
+ * @dev Extension of the ERC20 interface for a mintable and burnable token.
+ */
 interface IERC20Mintable is IERC20 {
     function mint(address _to, uint256 _amount) external;
 
     function burn(address _to, uint256 _amount) external;
 }
 
-contract magicDRAGON is ERC20, IERC20Mintable {
-    address public operator;
+/**
+ * @title drMAGIC
+ * @author kvk0x
+ *
+ * drMAGIC is a wrapped version of MAGIC, similar to cvxCRV in the Convex/Curve
+ * ecosystem. drMAGIC can be looked at as the "entry ticket" to capturing emissions
+ * from MDD's yield-generating activities. drMAGIC can be staked to earn blended yield,
+ * or LP'd with MAGIC to earn farming rewards.
+ *
+ */
+contract drMAGIC is ERC20, IERC20Mintable, AccessControl {
+    // ============================================ ROLES ==============================================
 
-    constructor() ERC20("Magic Dragon MAGIC", "dragonMAGIC") {
-        operator = msg.sender;
+    /// @dev Contract owner. Allowed to update access to other roles.
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+
+    /// @dev Role allowing minting of tokens. Will be granted to depositor contract.
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    /// @dev Role allowing burning of tokens. Not currently used, but may be in the future
+    ///      (e.g. for a hypothetical MAGIC redemption).
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+    // ========================================== CONSTRUCTOR ===========================================
+
+    /**
+     * @notice Initializes the roles of the contract, with a defined administrator,
+     *         who is allowed to set other administrators, minters, and burners.
+     */
+    constructor() ERC20("Dragon MAGIC", "drMAGIC") {
+        _setupRole(ADMIN_ROLE, msg.sender);
+
+        // Allow only admins to change other roles
+        _setRoleAdmin(ADMIN_ROLE, ADMIN_ROLE);
+        _setRoleAdmin(MINTER_ROLE, ADMIN_ROLE);
+        _setRoleAdmin(BURNER_ROLE, ADMIN_ROLE);
     }
 
-    function setOperator(address _operator) external {
-        require(msg.sender == operator, "Not operator");
-        operator = _operator;
-    }
+    // ======================================== TOKEN OPERATIONS ========================================
 
-    function mint(address _to, uint256 _amount) external {
-        require(msg.sender == operator, "Not operator");
-
+    /**
+     * @notice Mint new tokens. Can only be called by a designated minter.
+     * @dev    Per ERC20 spec, emits a Transfer event.
+     *
+     * @param _to                   The address to receive newly-minted tokens.
+     * @param _amount               The amount of tokens to mint.
+     */
+    function mint(address _to, uint256 _amount) external onlyRole(MINTER_ROLE) {
         _mint(_to, _amount);
     }
 
-    function burn(address _from, uint256 _amount) external {
-        require(msg.sender == operator, "Not operator");
-
+    /**
+     * @notice Burn tokens. Can only be called by a designated burner.
+     * @dev    Per ERC20 spec, emits a Transfer event.
+     *
+     * @param _from                 The address from which to debit burned tokens.
+     * @param _amount               The amount of tokens burn mint.
+     */
+    function burn(address _from, uint256 _amount) external onlyRole(BURNER_ROLE) {
         _burn(_from, _amount);
     }
 }
