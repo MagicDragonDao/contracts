583a3ab7eb5ce366b48b26cbc763cb9cca71846b
millenarian <kvk0x@protonmail.com>
2022-09-05 16:56:20 -0700

    do drmagic tests

--- test/drmagic/drMAGIC.ts
+++ test/drmagic/drMAGIC.ts
@@ -17,6 +17,8 @@ interface TestContext {
 
 const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 const MINTER_ROLE = "0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6";
+const BURNER_ROLE = "0x3c11d16cbaffd01df69ce1c404f6340ee057498f5f00246190ea54220576a848";
+const ADMIN_ROLE = "0xa49807205ce4d355092ef5a8a18f56e8913cf4a201fbe287825b095693c21775";
 
 describe("drMAGIC Token", () => {
     let ctx: TestContext;
@@ -68,25 +70,68 @@ describe("drMAGIC Token", () => {
             await expect(drmagic.connect(minter).mint(minter.address, amount))
                 .to.emit(drmagic, "Transfer")
                 .withArgs(ZERO_ADDRESS, minter.address, amount);
+
+            expect(await drmagic.balanceOf(minter.address)).to.eq(amount);
         });
     });
 
     describe("burn", () => {
-        beforeEach(async () => {
-            const { drmagic, admin, minter } = ctx;
+        it("does not allow a non-burner to burn", async () => {
+            const { drmagic, admin } = ctx;
+
+            await expect(drmagic.connect(admin).burn(admin.address, amount)).to.be.revertedWith("AccessControl");
+        });
+
+        it("does not allow a non-admin to grant burn permissions", async () => {
+            const { drmagic, burner } = ctx;
+
+            await expect(drmagic.connect(burner).grantRole(BURNER_ROLE, burner.address)).to.be.revertedWith(
+                "AccessControl",
+            );
+        });
+
+        it("allows an admin to grant burn permissions", async () => {
+            const { drmagic, admin, burner } = ctx;
+
+            await expect(drmagic.connect(admin).grantRole(BURNER_ROLE, burner.address)).to.emit(drmagic, "RoleGranted");
+        });
+
+        it("allows a burner to burn tokens", async () => {
+            const { drmagic, admin, minter, burner } = ctx;
 
             await drmagic.connect(admin).grantRole(MINTER_ROLE, minter.address);
             await drmagic.connect(minter).mint(minter.address, amount);
-        });
 
-        it("does not allow a non-burner to burn");
-        it("does not allow a non-admin to grant burn permissions");
-        it("allows an admin to grant burn permissions");
-        it("allows a burner to burn tokens");
+            expect(await drmagic.balanceOf(minter.address)).to.eq(amount);
+
+            await drmagic.connect(admin).grantRole(BURNER_ROLE, burner.address);
+
+            await expect(drmagic.connect(burner).burn(minter.address, amount))
+                .to.emit(drmagic, "Transfer")
+                .withArgs(minter.address, ZERO_ADDRESS, amount);
+
+            expect(await drmagic.balanceOf(minter.address)).to.eq(0);
+        });
     });
 
     describe("admin", () => {
-        it("does not allow a non-admin to grant the admin role");
-        it("allows an admin to grant the admin role");
+        it("does not allow a non-admin to grant the admin role", async () => {
+            const { drmagic, burner } = ctx;
+
+            await expect(drmagic.connect(burner).grantRole(ADMIN_ROLE, burner.address)).to.be.revertedWith(
+                "AccessControl",
+            );
+        });
+
+        it("allows an admin to grant the admin role", async () => {
+            const { drmagic, admin, minter } = ctx;
+
+            await expect(drmagic.connect(admin).grantRole(ADMIN_ROLE, minter.address)).to.emit(drmagic, "RoleGranted");
+
+            await expect(drmagic.connect(minter).grantRole(MINTER_ROLE, minter.address)).to.emit(
+                drmagic,
+                "RoleGranted",
+            );
+        });
     });
 });
